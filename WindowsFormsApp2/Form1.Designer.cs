3284ac99c1cc3a1b090aa9b7796e2fbfe0384217
Harmanpreet Singh <HS036@atcltd.atc.org.nz>
2019-02-14 15:08:34 -0800

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LVGolf = new System.Windows.Forms.ListView();
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Title = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Firstname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Surname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Gender = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Dob = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Street = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.City = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Suburb = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Available = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Handicap = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // LVGolf
+            // 
+            this.LVGolf.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.Title,
+            this.Firstname,
+            this.Surname,
+            this.Gender,
+            this.Dob,
+            this.Street,
+            this.City,
+            this.Suburb,
+            this.Available,
+            this.Handicap});
+            this.LVGolf.Location = new System.Drawing.Point(2, 0);
+            this.LVGolf.Name = "LVGolf";
+            this.LVGolf.Size = new System.Drawing.Size(833, 261);
+            this.LVGolf.TabIndex = 0;
+            this.LVGolf.UseCompatibleStateImageBehavior = false;
+            this.LVGolf.View = System.Windows.Forms.View.Details;
+            this.LVGolf.SelectedIndexChanged += new System.EventHandler(this.LVGolf_SelectedIndexChanged);
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            // 
+            // Title
+            // 
+            this.Title.Text = "Title";
+            // 
+            // Firstname
+            // 
+            this.Firstname.Text = "First Name";
+            this.Firstname.Width = 79;
+            // 
+            // Surname
+            // 
+            this.Surname.Text = "Surname";
+            // 
+            // Gender
+            // 
+            this.Gender.Text = "Gender";
+            // 
+            // Dob
+            // 
+            this.Dob.Text = "DOB";
+            // 
+            // Street
+            // 
+            this.Street.Text = "Street";
+            // 
+            // City
+            // 
+            this.City.Text = "City";
+            // 
+            // Suburb
+            // 
+            this.Suburb.Text = "Suburb";
+            // 
+            // Available
+            // 
+            this.Available.Text = "Available";
+            // 
+            // Handicap
+            // 
+            this.Handicap.Text = "Handicap";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 450);
+            this.Controls.Add(this.LVGolf);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView LVGolf;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.ColumnHeader Title;
+        private System.Windows.Forms.ColumnHeader Firstname;
+        private System.Windows.Forms.ColumnHeader Surname;
+        private System.Windows.Forms.ColumnHeader Gender;
+        private System.Windows.Forms.ColumnHeader Dob;
+        private System.Windows.Forms.ColumnHeader Street;
+        private System.Windows.Forms.ColumnHeader City;
+        private System.Windows.Forms.ColumnHeader Suburb;
+        private System.Windows.Forms.ColumnHeader Available;
+        private System.Windows.Forms.ColumnHeader Handicap;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LVGolf_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LVGolf.Items.Clear(); //clear out old data in the listbox, we will need this later
+                                  //load the connection string and pass it to the command
+            string connectionString = @"Data Source=DESKTOP-U551NOV\SQLEXPRESS;Initial Catalog=Golf;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string queryString = "SELECT * FROM golf ORDER by ID";
+                //read in the data with a datareader open the data connection
+                SqlCommand Command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = Command.ExecuteReader();
+                while (reader.Read())
+                {
+                    //add each row to the listbox
+                    ListViewItem item = new ListViewItem(new[] {
+reader["ID"].ToString(), reader["Title"].ToString(),
+reader["Firstname"].ToString(), reader["Surname"].ToString(),
+reader["Gender"].ToString(), reader["DOB"].ToString(), reader["Street"].ToString(),
+reader["Suburb"].ToString(), reader["City"].ToString(), reader["Available week days"].ToString(), reader["Handicap"].ToString()
+ });
+                    LVGolf.Items.Add(item);
+                }
+                reader.Close();
+            }
+        }
+    }
+}
